10d97afbce94eea7dc39dfd0d599e798b5101a56
Jake Lopez <jakedlopez@gmail.com>
2018-02-15 19:37:45 -0500

    pls for the love of god stop showing me a password

--- custom_users/serializers.py
+++ custom_users/serializers.py
@@ -2,16 +2,20 @@ from .models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('email', 'first_name', 'last_name', 'password', 'zip_code')
-
     password = serializers.CharField(
             min_length=8,
             max_length=128,
+            write_only=True
             )
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'password', 'zip_code')
+        extra_kwargs = { 'password' : { 'write_only' : True } }
+
     
     def create(self, validated_data):
         u =  User.objects.create(**validated_data)
         u.set_password(validated_data.get('password'))
+        u.save()
         return u
