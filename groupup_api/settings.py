09bea1ec2f75af4f42ba6fa81eb7e3394a2b580e
Jake Lopez <jakedlopez@gmail.com>
2018-02-20 08:24:22 -0500

    Updated settings for JWT token

--- groupup_api/settings.py
+++ groupup_api/settings.py
@@ -132,4 +132,10 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
     ),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    )
 }
