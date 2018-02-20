b6db5547cc765a59189cc6f3fcba13520fae9b38
Jake Lopez <jakedlopez@gmail.com>
2018-02-20 09:11:18 -0500

    Updated expiration date on JWT token

--- groupup_api/settings.py
+++ groupup_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=30),
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
